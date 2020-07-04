adeddc2ff04648240c226665b7de0fb517cd1b25
Chris Zuber <shgysk8zer0@gmail.com>
2020-07-04 09:25:36 -0700

    Update other dependencies and assets
    Update to be up-to-date with other projects

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+<!-- markdownlint-disable -->
+## [v1.1.0] - 2020-07-04
+
+### Added
+- CHANGELOG
+- Dependabot config
+- Super Linter
+- Minifying of JS & CSS
+- Add `<github-user>` to footer
+- `.well-known/assetlinks.json` for TWA support
+- Play Store app id for install prompt
+
+### Changed
+- Update service worker cache & config to use minified resources
+
+### Removed
+- Travis-CI config file (just use GitHub Actions)
+<!-- markdownlint-restore -->
--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
+import 'https://unpkg.com/@webcomponents/custom-elements@1.3.2/custom-elements.min.js';
 import 'https://cdn.kernvalley.us/components/share-button.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
-import 'https://cdn.kernvalley.us/components/gravatar-img.js';
-import 'https://cdn.kernvalley.us/components/login-button.js';
-import 'https://cdn.kernvalley.us/components/logout-button.js';
-import {registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
-
-if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
-	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
-}
+import 'https://cdn.kernvalley.us/components/github/user.js';
+import 'https://cdn.kernvalley.us/components/pwa/install.js';
+import 'https://cdn.kernvalley.us/components/weather-current.js';
+import 'https://cdn.kernvalley.us/components/weather-forecast.js';
+import 'https://cdn.kernvalley.us/components/ad-block.js';
 
 document.documentElement.classList.replace('no-js', 'js');
 document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);

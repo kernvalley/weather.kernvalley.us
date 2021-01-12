2e2b6a18c79ab3bf08b949eee88c4eefd570c243
Chris Zuber <shgysk8zer0@gmail.com>
2021-01-11 17:07:03 -0800

    Update list of cities
    Also updates footer

--- js/consts.js
+++ js/consts.js
@@ -1,8 +1,7 @@
 export const title = 'Weather';
+export const GA = 'UA-119717465-8';
 export const appId = '41ff0542577756523c5fcfaaeabe89d7';
-export const site = {
-	title,
-};
+export const site = { title };
 export const cities = {
 	lakeIsabella: {
 		name: 'Lake Isabella',
@@ -24,6 +23,20 @@ export const cities = {
 		postalCode: 93283,
 		key: 'weldon',
 	},
+	bodfish: {
+		name: 'Bodfish',
+		postalCode: 93205,
+		key: 'bodfish',
+	},
+	onyx: {
+		name: 'Onyx',
+		postalCode: 93255,
+		key: 'onyx',
+	},
+	caliente: {
+		name: 'Caliente',
+		postalCode: 93518,
+		key: 'caliente',
+	},
 };
 
-export const GA = 'UA-119717465-8';

f60a0a0e792ed52cf99ffa8dc5a56be399f1e28a
Chris Zuber <shgysk8zer0@gmail.com>
2023-07-05 06:51:02 -0700

    Update dependencies and config

--- sw-config.js
+++ sw-config.js
@@ -1,7 +1,7 @@
 /* eslint-env serviceworker */
 /* eslint no-unused-vars: 0 */
 const config = {
-	version: '2.0.0',
+	version: '2.0.1',
 	fresh: [
 		'https://apps.kernvalley.us/apps.json',
 		'/manifest.json',
@@ -9,24 +9,26 @@ const config = {
 	stale: [
 		/* HTML */
 		'/',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/weather/forecast.html',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/weather/current.html',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/github/user.html',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/pwa/prompt.html',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/krv/ad.html',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/install/prompt.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/weather/forecast.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/weather/current.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/github/user.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/pwa/prompt.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/krv/ad.html',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/install/prompt.html',
 
 		/* JavaScript */
 		'/js/index.min.js',
+		'https://unpkg.com/@shgysk8zer0/polyfills@0.2.0/all.min.js',
+		'https://unpkg.com/@shgysk8zer0/kazoo@0.2.0/harden.js',
 
 		/* Stylesheets */
 		'/css/index.min.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/weather/forecast.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/weather/current.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/github/user.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/pwa/prompt.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/krv/ad.css',
-		'https://unpkg.com/@shgysk8zer0/components@0.0.9/install/prompt.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/weather/forecast.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/weather/current.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/github/user.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/pwa/prompt.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/krv/ad.css',
+		'https://unpkg.com/@shgysk8zer0/components@0.2.0/install/prompt.css',
 
 		/* Images */
 		'/img/icons.svg',
